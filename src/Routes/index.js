19b421e1ac099d79ffd32b93cf0685e67ae9613a
Jusety <seva.venedikt0v@yandex.ru>
2023-04-30 01:15:18 +0300

    init

---
+++ src/Routes/index.js
@@ -0,0 +1,6 @@
+import Chat from "../components/Chat";
+import Login from "../components/Login";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "../utils/constants";
+
+export const publicRoutes = [{ path: LOGIN_ROUTE, element: <Login /> }];
+export const privateRoutes = [{ path: CHAT_ROUTE, element: <Chat /> }];
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { privateRoutes, publicRoutes } from "../Routes";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "../utils/constants";
+
+const AppRouter = () => {
+    const user = false;
+    return (
+        <div>
+            {user ? (
+                <Routes>
+                    {privateRoutes.map((route) => (
+                        <Route to={route.path} element={route.element} />
+                    ))}
+                    <Route
+                        path="*"
+                        element={<Navigate to={CHAT_ROUTE} replace />}
+                    />
+                </Routes>
+            ) : (
+                <Routes>
+                    {publicRoutes.map((route) => (
+                        <Route to={route.path} element={route.element} />
+                    ))}
+                    <Route
+                        path="*"
+                        element={<Navigate to={LOGIN_ROUTE} replace />}
+                    />
+                </Routes>
+            )}
+        </div>
+    );
+};
+
+export default AppRouter;
